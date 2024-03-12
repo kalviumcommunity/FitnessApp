553a769533ad97fe242e37b909257c9764ffac5d
kiruthik j <kiruthikj@kiruthiks-Laptop.local>
2024-03-12 11:03:13 +0530

    yes

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
+const port = 3000
 
-const port = process.env.PORT || 3000
 
 app.get("/", (res, req) => {
   res.send({ message: "Hello World!" });
