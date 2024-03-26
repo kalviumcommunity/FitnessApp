9b74bb4947345f57c1be39cb3fd56284708d7079
kiruthik j <kiruthikj@kiruthiks-Laptop.local>
2024-03-26 14:00:27 +0530

    linked page

--- Client/web/src/Components/SignIn.jsx
+++ Client/web/src/Components/SignIn.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 import './SignIn.css'; 
 
-const SignIn = () => {
+const SignIn = ({setIsLoggedIn}) => {
     const [formData, setFormData] = useState({
         username: '',
         password: ''
     });
+    const [error, setError] = useState('');
+    const Navigate = useNavigate();
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -19,12 +21,23 @@ const SignIn = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         
+        if (!formData.username || !formData.password) {
+            setError('Please enter both username and password.');
+            return;
+        }
+        
+        // If both username and password are provided, clear any existing error message.
+        setError('');
+
         console.log(formData); 
+        setIsLoggedIn(true);
+        Navigate("/");
     }
 
     return (
         <div className="sign-in-container">
             <h2 className="sign-in-title">Sign In</h2>
+            {error && <p className="error-message">{error}</p>}
             <form className="sign-in-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="username">Username:</label>
